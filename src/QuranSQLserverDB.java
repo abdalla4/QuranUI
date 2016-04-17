3b4aa5f21934ed1e24d742af0e6f2ce41a5b2571
abdalla4 <memiibrahim2434@gmail.com>
2016-04-16 17:00:23 -0700

    changes for 4/16/2016
    updated version of the UI, still working on sliders and fixing some
    bugs.

--- src/QuranSQLserverDB.java
+++ src/QuranSQLserverDB.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 public class QuranSQLserverDB {
 	private static Connection conn;
-	private List<QuranWordSim> list;
+	private List<QuranWordSim> list_WordSim;
+	private List<QuranCharSim> list_CharSim;
 	
 	public static void createConnection() throws SQLException {
 		String connectionUrl = "jdbc:sqlserver://localhost:1433;" +
-		         "databaseName=QuranV22;user=sa;password=aaa";
+		         "databaseName=QuranV22;user=sa;password=bbb";
 		try {
 			// Establish the connection.
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -25,16 +26,18 @@ public class QuranSQLserverDB {
 		}
 		
 		Statement stmt = null;
-		String query = "select s1.SurahName, a1.Ayah, s2.SurahName, a2.Ayah, sim.NoOfMatchingWords, sim.PercentageOfMatchingWordsInSentence1, sim.PercentageOfMatchingWordsInSentence2 "
-				+ "from dbo.[N1-Ayahs] a1, dbo.[N1-Ayahs] a2, dbo.[N1-Surahs] s1,  dbo.[N1-Surahs] s2, [dbo].[Similarity_Wordbased_NoOrder_NoRepetetion] sim"
-				+ "where sim.AyahSerialNo1 = a1.AyahSerialNo"
-				+ "and sim.AyahSerialNo2 = a2.AyahSerialNo"
-				+ "and a1.SurahSerialNo = s1.SurahSerialNo"
-				+ "and a2.SurahSerialNo = s2.SurahSerialNo"
-				+ "and sim.PercentageOfMatchingWordsInSentence1 > 60"
+		String query = "select /*top 1000*/ s1.SurahName, a1.Ayah, s2.SurahName, "
+				+ "a2.Ayah, sim.NoOfMatchingWords, "
+				+ "sim.PercentageOfMatchingWordsInSentence1, sim.PercentageOfMatchingWordsInSentence2 "
+				+ "from dbo.[N1-Ayahs] a1, dbo.[N1-Ayahs] a2, dbo.[N1-Surahs] s1,  dbo.[N1-Surahs] s2, [dbo].[Similarity_Wordbased_NoOrder_NoRepetetion] sim "
+				+ "where sim.AyahSerialNo1 = a1.AyahSerialNo "
+				+ "and sim.AyahSerialNo2 = a2.AyahSerialNo "
+				+ "and a1.SurahSerialNo = s1.SurahSerialNo "
+				+ "and a2.SurahSerialNo = s2.SurahSerialNo "
+				+ "and sim.PercentageOfMatchingWordsInSentence1 > 60 "
 				+ "and sim.PercentageOfMatchingWordsInSentence2 > 70";
 		
-		list = new ArrayList<QuranWordSim>();
+		list_WordSim = new ArrayList<QuranWordSim>();
 		try {
 			stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
@@ -48,7 +51,7 @@ public class QuranSQLserverDB {
 				int PercentageOfMatchingWordsInSentence2 = rs.getInt("PercentageOfMatchingWordsInSentence2");
 				
 				QuranWordSim x = new QuranWordSim(SurahName1, Ayah1, SurahName2, Ayah2, NoOfMatchingWords, PercentageOfMatchingWordsInSentence1, PercentageOfMatchingWordsInSentence2);
-				list.add(x);
+				list_WordSim.add(x);
 			}
 		} catch (SQLException e) {
 			System.out.println(e);
@@ -57,17 +60,60 @@ public class QuranSQLserverDB {
 				stmt.close();
 			}
 		}
-		return list;
+		return list_WordSim;
+	}
+	
+	public List<QuranCharSim> getQuery2() throws SQLException {
+		if (conn == null) {
+			createConnection();
+		}
+		
+		Statement stmt2 = null;
+		String query2 = "select /*top 1000*/ s1.SurahName, a1.Ayah, s2.SurahName, "
+				+ "a2.Ayah, sim.NoOfMatchingChars, "
+				+ "sim.PercentageOfMatchingCharsInSentence1, sim.PercentageOfMatchingCharsInSentence2 "
+				+ "from dbo.[N1-Ayahs] a1, dbo.[N1-Ayahs] a2, dbo.[N1-Surahs] s1,  dbo.[N1-Surahs] s2, [dbo].[Similarity_Charbased_NoOrder_NoRepetetion] sim "
+				+ "where sim.AyahSerialNo1 = a1.AyahSerialNo "
+				+ "and sim.AyahSerialNo2 = a2.AyahSerialNo "
+				+ "and a1.SurahSerialNo = s1.SurahSerialNo "
+				+ "and a2.SurahSerialNo = s2.SurahSerialNo "
+				+ "and sim.PercentageOfMatchingCharsInSentence1 > 90 "
+				+ "and sim.PercentageOfMatchingCharsInSentence2 > 70";
+		
+		list_CharSim = new ArrayList<QuranCharSim>();
+		try {
+			stmt2 = conn.createStatement();
+			ResultSet rs = stmt2.executeQuery(query2);
+			while (rs.next()) {
+				String SurahName1 = rs.getString("SurahName");
+				String Ayah1 = rs.getString("Ayah");
+				String SurahName2 = rs.getString("SurahName");
+				String Ayah2 = rs.getString("Ayah");
+				int NoOfMatchingChars = rs.getInt("NoOfMatchingChars");
+				int PercentageOfMatchingCharsInSentence1 = rs.getInt("PercentageOfMatchingCharsInSentence1");
+				int PercentageOfMatchingCharsInSentence2 = rs.getInt("PercentageOfMatchingCharsInSentence2");
+				
+				QuranCharSim y = new QuranCharSim(SurahName1, Ayah1, SurahName2, Ayah2, NoOfMatchingChars, PercentageOfMatchingCharsInSentence1, PercentageOfMatchingCharsInSentence2);
+				list_CharSim.add(y);
+			}
+		} catch (SQLException e) {
+			System.out.println(e);
+		} finally {
+			if (stmt2 != null) {
+				stmt2.close();
+			}
+		}
+		return list_CharSim;
 	}
 	
 	public List<QuranWordSim> getQuran(String qur) {
 		List<QuranWordSim> filterList = new ArrayList<QuranWordSim>();
 		try {
-			list = getQuery();
+			list_WordSim = getQuery();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		for (QuranWordSim h : list) {
+		for (QuranWordSim h : list_WordSim) {
 			if (h.getAyah1().toLowerCase().contains(qur.toLowerCase())) {
 				filterList.add(h);
 			}
@@ -75,30 +121,26 @@ public class QuranSQLserverDB {
 		return filterList;
 	}
 	
-	public void addproduct(QuranWordSim q) {
-//		String sql = "insert into dbo.Quran values " + "(?, ?, ?, ?, ?, ?); ";
-//
-//		PreparedStatement preparedStatement = null;
-//		try {
-//			preparedStatement = conn.prepareStatement(sql);
-//			preparedStatement.setInt(1, q.getAyahSerialNo());
-//			preparedStatement.setString(2, q.getAyah());
-//			preparedStatement.setInt(3, q.getSurahSerialNo());
-//			preparedStatement.setInt(4, q.getRubSerialNo());
-//			preparedStatement.setInt(5, q.getAyahNoWithSurah());
-//			preparedStatement.setInt(6, q.getAyahNoWithinRub());
-//			preparedStatement.executeUpdate();
-//		} catch (SQLException e) {
-//			System.out.println(e);
-//			e.printStackTrace();
-//		} 
+	public List<QuranCharSim> getQuran_forChar(String qur2) {
+		List<QuranCharSim> filterList2 = new ArrayList<QuranCharSim>();
+		try {
+			list_CharSim = getQuery2();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		for (QuranCharSim z : list_CharSim) {
+			if (z.getAyah1().toLowerCase().contains(qur2.toLowerCase())) {
+				filterList2.add(z);
+			}
+		}
+		return filterList2;
 	}
 	
 	public void update(int row, String columnName, Object data) {
-		QuranWordSim qu = list.get(row);
+		QuranWordSim qu = list_WordSim.get(row);
 		//String pName = product.getProductName();
-		int sNum = qu.getNoOfMatchingWords();
-		String sql = "update dbo.[N1-Ayahs] a1, dbo.[N1-Ayahs] a2, dbo.[N1-Surahs] s1,  dbo.[N1-Surahs] s2, [dbo].[Similarity_Wordbased_NoOrder_NoRepetetion] sim set " + columnName + " = ?  where sNum = ? ";
+		int matching = qu.getNoOfMatchingWords();
+		String sql = "update dbo.[N1-Ayahs] a1, dbo.[N1-Ayahs] a2, dbo.[N1-Surahs] s1,  dbo.[N1-Surahs] s2, [dbo].[Similarity_Wordbased_NoOrder_NoRepetetion] sim set " + columnName + " = ?  where matching = ? ";
 		System.out.println(sql);
 		PreparedStatement preparedStatement = null;
 		try {
@@ -107,8 +149,29 @@ public class QuranSQLserverDB {
 				preparedStatement.setString(1, (String) data);
 			else if (data instanceof Integer)
 				preparedStatement.setInt(1, (Integer) data);
-			preparedStatement.setInt(2, sNum);
+			preparedStatement.setInt(2, matching);
 			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e);
+			e.printStackTrace();
+		} 
+	}
+	
+	public void update_forChar(int row_forChar, String columnName_forChar, Object data_forChar) {
+		QuranCharSim qui = list_CharSim.get(row_forChar);
+		//String pName = product.getProductName();
+		int Char_matching = qui.getNoOfMatchingChars();
+		String Char_sql = "update dbo.[N1-Ayahs] a1, dbo.[N1-Ayahs] a2, dbo.[N1-Surahs] s1,  dbo.[N1-Surahs] s2, [dbo].[Similarity_Wordbased_NoOrder_NoRepetetion] sim set " + columnName_forChar + " = ?  where Char_matching = ? ";
+		System.out.println(Char_sql);
+		PreparedStatement Char_preparedStatement = null;
+		try {
+			Char_preparedStatement = conn.prepareStatement(Char_sql);
+			if (data_forChar instanceof String)
+				Char_preparedStatement.setString(1, (String) data_forChar);
+			else if (data_forChar instanceof Integer)
+				Char_preparedStatement.setInt(1, (Integer) data_forChar);
+			Char_preparedStatement.setInt(2, Char_matching);
+			Char_preparedStatement.executeUpdate();
 		} catch (SQLException e) {
 			System.out.println(e);
 			e.printStackTrace();
--- src/UI.java
+++ src/UI.java
@@ -3,8 +3,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.util.List;
-//import java.sql.SQLException;
-//import java.util.List;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeEvent;
@@ -12,33 +10,31 @@ import javax.swing.event.ChangeListener;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.TableModel;
-//import javax.swing.table.TableModel;
 
 public class UI extends JFrame implements ActionListener, TableModelListener, ChangeListener {
 	private static final long serialVersionUID = 1779520078061383929L;
-	private JRadioButton btn1;
-	private JRadioButton btn2;
-	private JSlider slider1A;
-	private JSlider slider2A;
-	private JSlider slider3A;
-	private JSlider slider1B;
-	private JSlider slider2B;
-	private JSlider slider3B;
+	private JRadioButton btn1_WordBased;
+	private JRadioButton btn2_CharBased;
+	private JSlider slider1A_V1_DegreeOfSim;
+	private JSlider slider2A_V1_DegreeOfConnectedness;
+	private JSlider slider3A_V2_DegreeOfSim;
+	private JSlider slider1B_V2_DegreeOfConnectedness;
+	private JSlider sliderC_OrderSensitivity;
 	private JButton btnRun;
-	private JLabel verse1;
-	private JLabel verse2;
+	private JLabel verse1_DegreeOfSim;
+	private JLabel verse2_DegreeOfSim;
 	private JLabel DOC_Verse1;
 	private JLabel DOC_Verse2;
 	private JLabel OrderS_Verse1;
-	private JLabel OrderS_Verse2;
 	
 	private JPanel contentPane;
 	private JPanel pnlContent;
 	
 	private QuranSQLserverDB db;
-	private List<QuranWordSim> list;
+	private List<QuranWordSim> list_WordSim;
+	private List<QuranCharSim> list_CharSim;
 	
-	private String[] columnNames = {"SurahName1",
+	private String[] Word_columnNames = {"SurahName1",
             "Ayah1",
             "SurahName2",
             "Ayah2",
@@ -46,9 +42,21 @@ public class UI extends JFrame implements ActionListener, TableModelListener, Ch
             "PercentageOfMatchingWordsInSentence1",
             "PercentageOfMatchingWordsInSentence2"};
 	
-	private Object[][] data;
-	private JTable table;
-	private JScrollPane scrollPane;
+	private Object[][] Word_data;
+	private JTable Word_table;
+	private JScrollPane Word_scrollPane;
+	
+	private String[] Char_columnNames = {"SurahName1",
+            "Ayah1",
+            "SurahName2",
+            "Ayah2",
+            "NoOfMatchingChars",
+            "PercentageOfMatchingCharsInSentence1",
+            "PercentageOfMatchingCharsInSentence2"};
+	
+	private Object[][] Char_data;
+	private JTable Char_table;
+	private JScrollPane Char_scrollPane;
 	
 	public UI() {
 		super("Quran UI");
@@ -56,16 +64,32 @@ public class UI extends JFrame implements ActionListener, TableModelListener, Ch
 		db = new QuranSQLserverDB();
 		
 		try {
-			list = db.getQuery();
-			data = new Object[list.size()][columnNames.length];
-			for (int i=0; i<list.size(); i++) {
-				data[i][0] = list.get(i).getSurahName1();
-				data[i][1] = list.get(i).getAyah1();
-				data[i][2] = list.get(i).getSurahName2();
-				data[i][3] = list.get(i).getAyah2();
-				data[i][4] = list.get(i).getNoOfMatchingWords();
-				data[i][5] = list.get(i).getPercentageOfMatchingWordsInSentence1();
-				data[i][6] = list.get(i).getPercentageOfMatchingWordsInSentence2();
+			list_WordSim = db.getQuery();
+			Word_data = new Object[list_WordSim.size()][Word_columnNames.length];
+			for (int i=0; i<list_WordSim.size(); i++) {
+				Word_data[i][0] = list_WordSim.get(i).getSurahName1();
+				Word_data[i][1] = list_WordSim.get(i).getAyah1();
+				Word_data[i][2] = list_WordSim.get(i).getSurahName2();
+				Word_data[i][3] = list_WordSim.get(i).getAyah2();
+				Word_data[i][4] = list_WordSim.get(i).getNoOfMatchingWords();
+				Word_data[i][5] = list_WordSim.get(i).getPercentageOfMatchingWordsInSentence1();
+				Word_data[i][6] = list_WordSim.get(i).getPercentageOfMatchingWordsInSentence2();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			list_CharSim = db.getQuery2();
+			Char_data = new Object[list_CharSim.size()][Char_columnNames.length];
+			for (int i=0; i<list_CharSim.size(); i++) {
+				Char_data[i][0] = list_CharSim.get(i).getSurahName1();
+				Char_data[i][1] = list_CharSim.get(i).getAyah1();
+				Char_data[i][2] = list_CharSim.get(i).getSurahName2();
+				Char_data[i][3] = list_CharSim.get(i).getAyah2();
+				Char_data[i][4] = list_CharSim.get(i).getNoOfMatchingChars();
+				Char_data[i][5] = list_CharSim.get(i).getPercentageOfMatchingCharsInSentence1();
+				Char_data[i][6] = list_CharSim.get(i).getPercentageOfMatchingCharsInSentence2();
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -82,145 +106,164 @@ public class UI extends JFrame implements ActionListener, TableModelListener, Ch
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		btn1 = new JRadioButton("Word Based");
-		btn1.setBounds(98, 19, 109, 23);
-		contentPane.add(btn1);
+		btn1_WordBased = new JRadioButton("Word Based");
+		btn1_WordBased.setBounds(98, 19, 109, 23);
+		contentPane.add(btn1_WordBased);
 		
-		btn2 = new JRadioButton("Character Based");
-		btn2.setBounds(220, 19, 109, 23);
-		contentPane.add(btn2);
+		btn2_CharBased = new JRadioButton("Character Based");
+		btn2_CharBased.setBounds(220, 19, 148, 23);
+		contentPane.add(btn2_CharBased);
 		
-		slider1A = new JSlider();
-		slider1A.setBounds(10, 82, 200, 23);
-		slider1A.setMajorTickSpacing(20);
-		slider1A.setMinorTickSpacing(10);
-		slider1A.setPaintTicks(true);
-		slider1A.addChangeListener(this);
-		contentPane.add(slider1A);
+		slider1A_V1_DegreeOfSim = new JSlider();
+		slider1A_V1_DegreeOfSim.setBounds(10, 98, 200, 23);
+		slider1A_V1_DegreeOfSim.setMajorTickSpacing(20);
+		slider1A_V1_DegreeOfSim.setMinorTickSpacing(10);
+		slider1A_V1_DegreeOfSim.setPaintTicks(true);
+		slider1A_V1_DegreeOfSim.addChangeListener(this);
+		contentPane.add(slider1A_V1_DegreeOfSim);
 		
-		slider2A = new JSlider();
-		slider2A.setBounds(10, 135, 200, 23);
-		slider2A.setMajorTickSpacing(20);
-		slider2A.setMinorTickSpacing(10);
-		slider2A.setPaintTicks(true);
-		slider2A.addChangeListener(this);
-		contentPane.add(slider2A);
+		slider2A_V1_DegreeOfConnectedness = new JSlider();
+		slider2A_V1_DegreeOfConnectedness.setBounds(10, 173, 200, 23);
+		slider2A_V1_DegreeOfConnectedness.setMajorTickSpacing(20);
+		slider2A_V1_DegreeOfConnectedness.setMinorTickSpacing(10);
+		slider2A_V1_DegreeOfConnectedness.setPaintTicks(true);
+		slider2A_V1_DegreeOfConnectedness.addChangeListener(this);
+		contentPane.add(slider2A_V1_DegreeOfConnectedness);
 		
-		slider3A = new JSlider();
-		slider3A.setBounds(10, 186, 200, 23);
-		slider3A.setMajorTickSpacing(20);
-		slider3A.setMinorTickSpacing(10);
-		slider3A.setPaintTicks(true);
-		slider3A.addChangeListener(this);
-		contentPane.add(slider3A);
+		slider3A_V2_DegreeOfSim = new JSlider();
+		slider3A_V2_DegreeOfSim.setBounds(114, 263, 200, 23);
+		slider3A_V2_DegreeOfSim.setMajorTickSpacing(20);
+		slider3A_V2_DegreeOfSim.setMinorTickSpacing(10);
+		slider3A_V2_DegreeOfSim.setPaintTicks(true);
+		slider3A_V2_DegreeOfSim.addChangeListener(this);
+		contentPane.add(slider3A_V2_DegreeOfSim);
 		
-		slider1B = new JSlider();
-		slider1B.setBounds(220, 82, 200, 23);
-		slider1B.setMajorTickSpacing(20);
-		slider1B.setMinorTickSpacing(10);
-		slider1B.setPaintTicks(true);
-		slider1B.addChangeListener(this);
-		contentPane.add(slider1B);
+		slider1B_V2_DegreeOfConnectedness = new JSlider();
+		slider1B_V2_DegreeOfConnectedness.setBounds(220, 98, 200, 23);
+		slider1B_V2_DegreeOfConnectedness.setMajorTickSpacing(20);
+		slider1B_V2_DegreeOfConnectedness.setMinorTickSpacing(10);
+		slider1B_V2_DegreeOfConnectedness.setPaintTicks(true);
+		slider1B_V2_DegreeOfConnectedness.addChangeListener(this);
+		contentPane.add(slider1B_V2_DegreeOfConnectedness);
 		
-		slider2B = new JSlider();
-		slider2B.setBounds(220, 135, 200, 23);
-		slider2B.setMajorTickSpacing(20);
-		slider2B.setMinorTickSpacing(10);
-		slider2B.setPaintTicks(true);
-		slider2B.addChangeListener(this);
-		contentPane.add(slider2B);
-		
-		slider3B = new JSlider();
-		slider3B.setBounds(220, 186, 200, 23);
-		slider3B.setMajorTickSpacing(20);
-		slider3B.setMinorTickSpacing(10);
-		slider3B.setPaintTicks(true);
-		slider3B.addChangeListener(this);
-		contentPane.add(slider3B);
+		sliderC_OrderSensitivity = new JSlider();
+		sliderC_OrderSensitivity.setBounds(220, 173, 200, 23);
+		sliderC_OrderSensitivity.setMajorTickSpacing(20);
+		sliderC_OrderSensitivity.setMinorTickSpacing(10);
+		sliderC_OrderSensitivity.setPaintTicks(true);
+		sliderC_OrderSensitivity.addChangeListener(this);
+		contentPane.add(sliderC_OrderSensitivity);
 		
 		btnRun = new JButton("RUN");
 		btnRun.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-//				int a = slider1A.getValue();
-//				int b = slider2A.getValue();
-//				int c = slider3A.getValue();
-//				int d = slider1B.getValue();
-//				int g = slider2B.getValue();
-//				int h = slider3B.getValue();
+				if (btn1_WordBased.isSelected()) {
+					try {
+						list_WordSim = db.getQuery();
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					Word_data = new Object[list_WordSim.size()][Word_columnNames.length];
+					for (int i=0; i<list_WordSim.size(); i++) {
+						Word_data[i][0] = list_WordSim.get(i).getSurahName1();
+						Word_data[i][1] = list_WordSim.get(i).getAyah1();
+						Word_data[i][2] = list_WordSim.get(i).getSurahName2();
+						Word_data[i][3] = list_WordSim.get(i).getAyah2();
+						Word_data[i][4] = list_WordSim.get(i).getNoOfMatchingWords();
+						Word_data[i][5] = list_WordSim.get(i).getPercentageOfMatchingWordsInSentence1();
+						Word_data[i][6] = list_WordSim.get(i).getPercentageOfMatchingWordsInSentence2();
+					}
+					pnlContent.removeAll();
+					Word_table = new JTable(Word_data, Word_columnNames);
+					//Word_table.getModel().addTableModelListener(this);
+					Word_scrollPane = new JScrollPane(Word_table);
+					pnlContent.add(Word_scrollPane);
+					pnlContent.revalidate();
+					//this.repaint();
+				} 
+				
+				if(btn2_CharBased.isSelected() && slider1A_V1_DegreeOfSim.getValue() == 100){
+					try {
+						list_CharSim = db.getQuery2();
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					Char_data = new Object[list_CharSim.size()][Char_columnNames.length];
+					for (int i=0; i<list_CharSim.size(); i++) {
+						Char_data[i][0] = list_CharSim.get(i).getSurahName1();
+						Char_data[i][1] = list_CharSim.get(i).getAyah1();
+						Char_data[i][2] = list_CharSim.get(i).getSurahName2();
+						Char_data[i][3] = list_CharSim.get(i).getAyah2();
+						Char_data[i][4] = list_CharSim.get(i).getNoOfMatchingChars();
+						Char_data[i][5] = list_CharSim.get(i).getPercentageOfMatchingCharsInSentence1();
+						Char_data[i][6] = list_CharSim.get(i).getPercentageOfMatchingCharsInSentence2();
+					}
+					pnlContent.removeAll();
+					Char_table = new JTable(Char_data, Char_columnNames);
+					//Char_table.getModel().addTableModelListener(this);
+					Char_scrollPane = new JScrollPane(Char_table);
+					pnlContent.add(Char_scrollPane);
+					pnlContent.revalidate();
+					//this.repaint();
+				} 
+				
+				if (btn1_WordBased.isSelected() && btn2_CharBased.isSelected()){
+					String error_message1 = "Please select one button at a time.";
+					JOptionPane.showMessageDialog(new JFrame(), error_message1, "Error Message", JOptionPane.ERROR_MESSAGE);
+				}
+				
+				if (!btn1_WordBased.isSelected() && !btn2_CharBased.isSelected()){
+					String error_message2 = "Please select a button.";
+					JOptionPane.showMessageDialog(new JFrame(), error_message2, "Error Message", JOptionPane.ERROR_MESSAGE);
+					
+				}
 			}
 		});
-		btnRun.setBounds(169, 228, 89, 23);
+		btnRun.setBounds(171, 331, 89, 23);
 		contentPane.add(btnRun);
 		
-		verse1 = new JLabel("Verse 1");
-		verse1.setBounds(93, 57, 46, 14);
-		contentPane.add(verse1);
+		verse1_DegreeOfSim = new JLabel("V1 Degree of Similarity");
+		verse1_DegreeOfSim.setBounds(60, 73, 148, 14);
+		contentPane.add(verse1_DegreeOfSim);
 		
-		verse2 = new JLabel("Verse 2");
-		verse2.setBounds(302, 57, 46, 14);
-		contentPane.add(verse2);
+		verse2_DegreeOfSim = new JLabel("V2 Degree of Similarity");
+		verse2_DegreeOfSim.setBounds(264, 73, 140, 14);
+		contentPane.add(verse2_DegreeOfSim);
 		
-		DOC_Verse1 = new JLabel("Degree Of Conn");
-		DOC_Verse1.setBounds(67, 116, 89, 14);
+		DOC_Verse1 = new JLabel("Degree Of Connectedness");
+		DOC_Verse1.setBounds(46, 148, 162, 14);
 		contentPane.add(DOC_Verse1);
 		
-		DOC_Verse2 = new JLabel("Degree Of Conn");
-		DOC_Verse2.setBounds(281, 116, 89, 14);
+		DOC_Verse2 = new JLabel("Degree Of Connectedness");
+		DOC_Verse2.setBounds(250, 148, 156, 14);
 		contentPane.add(DOC_Verse2);
 		
 		OrderS_Verse1 = new JLabel("Order Sensitivity");
-		OrderS_Verse1.setBounds(67, 169, 89, 14);
+		OrderS_Verse1.setBounds(171, 238, 109, 14);
 		contentPane.add(OrderS_Verse1);
 		
-		OrderS_Verse2 = new JLabel("Order Sensitivity");
-		OrderS_Verse2.setBounds(281, 169, 89, 14);
-		contentPane.add(OrderS_Verse2);
-		
 		pnlContent = new JPanel();
-		pnlContent.setBounds(448, 19, 458, 224);
-		table = new JTable(data, columnNames);
-		scrollPane = new JScrollPane(table);
-		pnlContent.add(scrollPane);
-		table.getModel().addTableModelListener(this);
+		pnlContent.setBounds(456, 19, 525, 596);
+		Word_table = new JTable(Word_data, Word_columnNames);
+		Word_scrollPane = new JScrollPane(Word_table);
+		pnlContent.add(Word_scrollPane);
+		Word_table.getModel().addTableModelListener(this);
 		contentPane.add(pnlContent);
 	}
 	
 	public static void main(String[] args) {
 		UI GUI = new UI();
 		GUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		GUI.setMinimumSize(new Dimension(1000, 320));
+		GUI.setMinimumSize(new Dimension(1000, 500));
 		GUI.setVisible(true);
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == btnRun) {
-			try {
-				list = db.getQuery();
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			data = new Object[list.size()][columnNames.length];
-			for (int i=0; i<list.size(); i++) {
-				data[i][0] = list.get(i).getSurahName1();
-				data[i][1] = list.get(i).getAyah1();
-				data[i][2] = list.get(i).getSurahName2();
-				data[i][3] = list.get(i).getAyah2();
-				data[i][4] = list.get(i).getNoOfMatchingWords();
-				data[i][5] = list.get(i).getPercentageOfMatchingWordsInSentence1();
-				data[i][6] = list.get(i).getPercentageOfMatchingWordsInSentence2();
-			}
-			pnlContent.removeAll();
-			table = new JTable(data, columnNames);
-			table.getModel().addTableModelListener(this);
-			scrollPane = new JScrollPane(table);
-			pnlContent.add(scrollPane);
-			pnlContent.revalidate();
-			this.repaint();
-		}
-	
+		
 	} 
 	
 	@Override
